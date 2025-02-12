b7cb32c2bca5f39f9d46df7378d73d7926193d04
Utkarsh Raj <utkarshjha879@gmail.com>
2025-02-12 18:11:03 +0530

    Delete vendor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { AppConfig } from './config/app.config';
 import { SidebarService } from './core/services/sidebar.service';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SVG_ICONS } from './config/asset.config';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +16,10 @@ import { SidebarService } from './core/services/sidebar.service';
 export class AppComponent {
   title = 'costing-ui';
 
-  constructor(private router: Router, private sidebarService: SidebarService) {
+  constructor(private router: Router, private sidebarService: SidebarService, 
+    private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
     this.handleRouteChange();
+    this.registerIcons();
   }
 
   handleRouteChange() {
@@ -28,6 +33,13 @@ export class AppComponent {
           return false;
         });
       }
+    });
+  }
+
+  registerIcons() {
+    SVG_ICONS.forEach(icon => {
+      console.log(icon);
+      this.iconRegistry.addSvgIcon(icon.name, this.sanitizer.bypassSecurityTrustResourceUrl(icon.path));
     });
   }
 }
--- src/app/modules/vendors/components/vendor-landing/vendor-landing.component.ts
+++ src/app/modules/vendors/components/vendor-landing/vendor-landing.component.ts
@@ -1,32 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, EventEmitter, Output } from '@angular/core';
 import { Vendor } from '../../../../data/models/vendor';
 import { VENDOR_TABLE_COLUMNS } from '../../../../data/constants/vendor-table-config.constants';
 import { VendorService } from '../../../../data/services/vendor/vendor.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCloseResponse } from '../../../../shared/constants/dialog.constants';
 import { Router } from '@angular/router';
-
+import { DiscardDialogComponent } from '../../../../shared/components/discard-dialog/discard-dialog.component';
+import { TableActions } from '../../../../shared/constants/table.constants';
 @Component({
   selector: 'app-vendor-landing',
   templateUrl: './vendor-landing.component.html',
-  styleUrl: './vendor-landing.component.scss'
+  styleUrls: ['./vendor-landing.component.scss']
 })
+
 export class VendorLandingComponent {
   vendorList: Vendor[] = [];
   columns: any[] = VENDOR_TABLE_COLUMNS;
   readonly dialog = inject(MatDialog);
 
+  @Output() actionTriggered = new EventEmitter<{ action: string; row: any }>();
+
   constructor(private vendorService: VendorService, private router: Router) {
     this.getVendorList();
   }
 
-  getVendorList() {
+  getVendorList(): void {
     this.vendorService.getVendorList().subscribe(res => {
       this.vendorList = res;
+    
+    (err: any) => {
+      console.error('Failed to fetch vendors:', err);
+    }
     });
   }
 
-  openCreateVendorDialog() {
+  openCreateVendorDialog(): void {
+    // Implementation for opening the create vendor dialog
+    // For example:
     // const dialogRef = this.dialog.open(VendorDialogComponent, {
     //   panelClass: ['app-dialog'],
     //   disableClose: true
@@ -38,27 +48,64 @@ export class VendorLandingComponent {
     // });
   }
 
-  createVendor() {
-    this.router.navigateByUrl("/app/vendors/create");
+  createVendor(): void {
+    this.router.navigateByUrl('/app/vendors/create');
   }
 
-  handleAction(event: { action: string; row: any }) {
+  handleAction(event: { action: TableActions; row: any }): void {
     const { action, row } = event;
-    if (action === 'edit') {
+
+    if (action === TableActions.DELETE) {
+      this.openDiscardDialog(row);
+    } else if (action === TableActions.EDIT) {
       this.router.navigateByUrl(`/app/vendors/edit/${row.id}`);
-    }else if (action === 'delete') {
-      this.deleteVendor(row.id);
+    } else {
+      this.actionTriggered.emit({ action, row });
     }
   }
-  deleteVendor(vendorId: string) {
+
+  openDiscardDialog(row: any): void {
+    const dialogRef = this.dialog.open(DiscardDialogComponent, {
+      width: '600px',
+      data: {
+        row
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.vendorService.deleteVendor(row.id.toString()).subscribe({
+          next: () => {
+            console.log('Vendor deleted successfully:', row);
+            this.getVendorList();
+          },
+          error: (error: any) => {
+            console.error('Error deleting vendor:', error);
+            alert('Failed to delete vendor.');
+          }
+        });
+      }
+    });
+  }
+
+  editRow(row: any): void {
+    console.log('Editing row:', row);
+    this.router.navigateByUrl(`/app/vendors/edit/${row.partId}`);
+  }
+
+  
+  deleteVendor(vendorId: string): void {
     if (confirm('Are you sure you want to delete this vendor?')) {
-      this.vendorService.deleteVendor(vendorId).subscribe(() => {
-        alert('Vendor deleted successfully.');
-        this.getVendorList();
-      }, (error: any) => {
-        console.error('Error deleting vendor:', error);
-        alert('Failed to delete vendor.');
-      });
+      this.vendorService.deleteVendor(vendorId).subscribe(
+        () => {
+          alert('Vendor deleted successfully.');
+          this.getVendorList();
+        },
+        (error: any) => {
+          console.error('Error deleting vendor:', error);
+          alert('Failed to delete vendor.');
+        }
+      );
     }
   }
 }
