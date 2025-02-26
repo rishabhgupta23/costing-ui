500fcb66b3a8dbbdf21dd452c21b1432fee84684
yash-agarwal12 <96440746+yash-agarwal12@users.noreply.github.com>
2025-02-26 14:51:59 +0530

    bom pagination done

--- src/app/modules/parts/components/bomdialog/bomdialog.component.ts
+++ src/app/modules/parts/components/bomdialog/bomdialog.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PartBomData, PartRow } from '../../../../data/models/part';
 import { PartService } from '../../../../data/services/part/part.service';
 import { DialogCloseResponse } from '../../../../shared/constants/dialog.constants';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-bomdialog',
@@ -13,8 +14,12 @@ export class BomdialogComponent {
   displayedColumns: string[] = ['select', 'partName', 'partNumber'];
   existingParts: Set<number>= new Set();
   partList: PartRow[] = [];
-   currentPage=0;
-   pageSize=100;
+  paginatedData: any[] = []; // Data to display on the current page
+  pageSize: number = 5 // Default items per page
+  currentPage: number = 0;
+  totalRecords: number=0;
+  pageInfo: any;
+  allParts: PartRow[] = [];
 
 constructor(
   public dialogRef: MatDialogRef<BomdialogComponent>,
@@ -45,23 +50,29 @@ getPartList():void{
     (response) => {
       this.partList = response.data?.partsList || [];
       this.existingParts = this.data.existingParts;
+      this.paginatedData = this.partList;
+      this.totalRecords = response.pageInfo?.totalRecords || 0;
+      this.allParts = [...this.allParts, ...this.partList];
+      this.allParts = Array.from(new Set(this.allParts.map(part => part.partId)))
+        .map(id => this.allParts.find(part => part.partId === id)!);
 });
 
 }
 
 isAllSelected(): boolean {
-  return this.partList.length > 0 && this.existingParts.size === this.partList.length;
+  return this.partList.length > 0 && this.partList.every(part => this.existingParts.has(part.partId));
 }
 
 isIndeterminate(): boolean {
-  return this.existingParts.size > 0 && this.existingParts.size < this.partList.length;
+  return this.partList.some(part => this.existingParts.has(part.partId)) &&
+         !this.isAllSelected();
 }
 
 selectAll(event: any): void {
   if (event.checked) {
     this.partList.forEach(part => this.existingParts.add(part.partId));
   } else {
-    this.existingParts.clear();
+    this.partList.forEach(part => this.existingParts.delete(part.partId));
   }
 }
 
@@ -70,8 +81,20 @@ checkIfSelected(part: PartRow) {
 }
 
 confirmSelection(): void {
-  const result  = this.partList.filter(part => this.existingParts.has(part.partId));
+  const result  = this.allParts.filter(part => this.existingParts.has(part.partId));
   this.dialogRef.close({data: result, action: DialogCloseResponse.UPDATE});
 }
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.getPartList();
+  }
+
+  updatePaginatedData() {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedData = this.partList.slice(startIndex, endIndex);
+  }
 
 }
