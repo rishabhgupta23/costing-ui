f2a849cd2f94cdafd5f290c5f83453e4c5c53d23
vidishaaaaa <btech15071.21@bitmesra.ac.in>
2025-03-06 13:10:50 +0530

    REMOVED COMMENT

--- src/app/data/services/cost-calculator/cost-calculator.service.ts
+++ src/app/data/services/cost-calculator/cost-calculator.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class CostCalculatorService {
     getCost(partId:number,mode:string): Observable<any> {
-      const url= `http://localhost:8080/cost/calculate/${partId}?&priceMode=${mode}`;
+      const url= `http://localhost:8081/cost/calculate/${partId}?&priceMode=${mode}`;
       return  this.http.get<any>(url);
     }
 
