6042d9d6d801173c4bfb8c2c357aa460079d3707
vidishaaaaa <btech15071.21@bitmesra.ac.in>
2025-03-06 13:26:15 +0530

    changes done

--- src/app/data/services/cost-calculator/cost-calculator.service.ts
+++ src/app/data/services/cost-calculator/cost-calculator.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ApiUtil } from '../../../shared/utils/api.util';
+import { API_END_POINTS } from '../../../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CostCalculatorService {
     getCost(partId:number,mode:string): Observable<any> {
-      const url= `http://localhost:8081/cost/calculate/${partId}?&priceMode=${mode}`;
+      const params = new Map<string, string>();
+    
+      const url = ApiUtil.getPreparedUrl(API_END_POINTS.COST_CALCULATE, params);
       return  this.http.get<any>(url);
     }
 
