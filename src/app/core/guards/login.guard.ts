88ef89e2a53b1fe17ea410101adc1824b3c9005b
yash-agarwal12 <96440746+yash-agarwal12@users.noreply.github.com>
2025-06-26 17:14:16 +0530

    login bugfix

---
+++ src/app/core/guards/login.guard.ts
@@ -0,0 +1,19 @@
+// login.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      this.router.navigate(['/app']);
+      return false;
+    }
+    return true;
+  }
+}
